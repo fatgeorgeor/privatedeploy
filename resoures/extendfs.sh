b5f6be7e1f31f54faf3be5a6321565b0fe62eee6
Dongmao Zhang <deanraccoon@gmail.com>
2018-01-29 13:08:00 +0800

    Fix extendfs.sh

--- resoures/extendfs.sh
+++ resoures/extendfs.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 function usage() {
-	echo "If root/var is too small, use this script to extend logic volume"
-	echo "extendfs.sh <DEV NAME>"
+        echo "If root/var is too small, use this script to extend logic volume"
+        echo "extendfs.sh <DEV NAME>"
 }
 
 if [[ $# != 1 ]]
 then
-	usage
-	exit -1
+        usage
+        exit -1
 fi
 
 DEVNAME=$1
 
 pvcreate ${DEVNAME}
-vgextend centos ${DEVNAME}
+vgextend systemVG ${DEVNAME}
 
-lvextend -L+500G /dev/centos/root
+lvextend -l+50%FREE /dev/systemVG/LVRoot
 xfs_growfs /
 
-lvextend -L+500G /dev/centos/var
+lvextend -l+100%FREE /dev/systemVG/var
 xfs_growfs /var
